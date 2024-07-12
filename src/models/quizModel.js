42b23fbbe628e89e9293a876cb58c1c8e4d4b11a
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-07-12 21:13:26 +0700

    features check the previous quiz added

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -45,25 +45,73 @@ async function GetAllQuizMateriToDB(id_materi) {
   }
 }
 
+async function CheckTheTakenQuiz(data) {
+  try {
+    const { id_materi, id_user } = data;
+
+    const queryTextMQ = `
+      SELECT materi.tingkat, materi.phase
+      FROM mengambilquiz
+      LEFT JOIN kumpulansoalquiz ON kumpulansoalquiz.id = mengambilquiz.quiz
+      LEFT JOIN materi ON kumpulansoalquiz.id_materi = materi.id
+      WHERE mengambilquiz.usernasho = $1 AND mengambilquiz.status = 'lulus'
+      ORDER BY mengambilquiz.created_at DESC, materi.tingkat ASC
+      LIMIT 1
+    `;
+
+    const queryValuesMQ = [id_user];
+    const usermaxujian = await pool.query(queryTextMQ, queryValuesMQ);
+
+    let userMaxTingkat = { phase: 1, tingkat: 1 };
+
+    if (usermaxujian.rows.length > 0) {
+      userMaxTingkat = usermaxujian.rows[0];
+    }
+
+    const queryTextQuizDiambil = `
+      SELECT tingkat, phase
+      FROM materi
+      WHERE id = $1
+    `;
+    const queryValuesQuizDiambil = [id_materi];
+    const QuizDiambil = await pool.query(queryTextQuizDiambil, queryValuesQuizDiambil);
+
+    if (userMaxTingkat.phase >= QuizDiambil.rows[0].phase) {
+      return userMaxTingkat.tingkat >= QuizDiambil.rows[0].tingkat;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    handleCustomErrorModel(error);
+  }
+}
+
 async function AddTakeQuizUserToDB(data) {
   try {
     const { id_materi, id_user } = data;
 
     const queryTextKS = `
-            SELECT id from kumpulansoalquiz WHERE id_materi = $1;
-        `;
+      SELECT id FROM kumpulansoalquiz WHERE id_materi = $1;
+    `;
     const queryValuesKS = [id_materi];
-
     const KumpulanSoalDB = await pool.query(queryTextKS, queryValuesKS);
 
-    const queryText = `
-        INSERT INTO public.mengambilquiz(
-            usernasho, quiz)
-            VALUES ($1, $2)
-            RETURNING id;
-        `;
+    if (KumpulanSoalDB.rows.length === 0) {
+      throw new CustomError(404, "Quiz not found for the given materi");
+    }
 
-    const queryValues = [id_user, KumpulanSoalDB.rows[0].id]; // Correct order of parameters
+    const isQuizAvailable = await CheckTheTakenQuiz(data);
+
+    if (!isQuizAvailable) {
+      throw new CustomError(401, "Get The Previously Quiz First");
+    }
+
+    const queryText = `
+      INSERT INTO public.mengambilquiz(usernasho, quiz)
+      VALUES ($1, $2)
+      RETURNING id;
+    `;
+    const queryValues = [id_user, KumpulanSoalDB.rows[0].id];
 
     const { rows } = await pool.query(queryText, queryValues);
     return rows;
@@ -104,31 +152,30 @@ async function GetNilaiQuizToDB(data) {
     const maxPoints = 100;
     const points = (trueCount / totalQuestions) * maxPoints;
 
-    let status = 'tidak lulus'
+    let status = "tidak lulus";
 
     if (Math.ceil(points) > 70) {
-      status = 'lulus'
-
+      status = "lulus";
     }
 
     const queryTextAddNilai = `
       UPDATE public.mengambilquiz
       SET nilai=$1, status=$2
       WHERE id=$3;
-    `
+    `;
 
-    const queryValuesAddNilai = [Math.ceil(points),status,id_mengambil_quiz]
+    const queryValuesAddNilai = [Math.ceil(points), status, id_mengambil_quiz];
 
-    const AddNilaiToDB = await pool.query(queryTextAddNilai,queryValuesAddNilai)
-    let status_kelulusan = false
-    if (status == 'lulus') {
-      status_kelulusan = true
+    const AddNilaiToDB = await pool.query(queryTextAddNilai, queryValuesAddNilai);
+    let status_kelulusan = false;
+    if (status == "lulus") {
+      status_kelulusan = true;
     }
 
     const payload = {
       nilai: Math.ceil(points),
-      lulus: status_kelulusan
-    }
+      lulus: status_kelulusan,
+    };
 
     return payload;
   } catch (error) {
